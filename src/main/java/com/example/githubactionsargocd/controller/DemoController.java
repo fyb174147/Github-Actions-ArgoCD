eca7e4af2d8c08ee2a16a7769d3f162a3c0945c0
fyb174147 <fyb174146@gmail.com>
2025-12-15 08:04:02 +0700

    update

--- src/main/java/com/example/githubactionsargocd/controller/DemoController.java
+++ src/main/java/com/example/githubactionsargocd/controller/DemoController.java
@@ -8,6 +8,6 @@ public class DemoController {
 
     @GetMapping("/")
     public String hello() {
-        return "Xin chào! Đây là Spring Boot với ArgoCD v2.";
+        return "Xin chào! Đây là Spring Boot với ArgoCD v3.";
     }
 }
